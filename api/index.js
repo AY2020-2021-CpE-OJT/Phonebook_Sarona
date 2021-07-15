0abdec559262dc87f14f5e4f6b4c7a65db0c4225
RSarona <saronaraulen@gmail.com>
2021-07-15 19:28:48 +0800

    Added all Functionalities from the previous API

--- api/index.js
+++ api/index.js
@@ -17,6 +17,53 @@ app.get('/', async (req, res) => {
     }
 });
 
+//GET BY ID
+app.get('/:id', async (req, res) => {
+    try {
+        User_Infos.findById({_id: req.params.id}).then(function(fetchedInfo){
+            res.json({fetchedInfo});
+        });
+        
+    }catch(err) {
+        res.json({message: err})
+    }
+});
+
+//SAVE DATA TO DB
+app.post('/', async (req,res) => {
+    const postInfo = new User_Infos({               //Uses the model and at the same time get info
+        last_name: req.body.last_name,
+        first_name: req.body.first_name,
+        phone_numbers: req.body.phone_numbers
+    });
+    try {
+        const savedInfo = await postInfo.save();
+        res.json(savedInfo);
+    }catch(err) {
+        res.json( {message: err} );
+    }
+});
+
+//DELETE DATA FROM DB
+app.delete('/:id', async (req,res) => {
+    try {
+        User_Infos.findByIdAndDelete({_id: req.params.id}).then(function(deletedInfo){
+            res.json(deletedInfo);
+        });
+    }catch(err) {
+        res.json( {message: err} );
+    }
+}),
+
+//UPDATE DATA FROM DB
+app.put('/:id', function(req, res, next){
+    User_Infos.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+        User_Infos.findOne({_id: req.params.id}).then(function(updatedInfo){
+            res.send(updatedInfo);
+        });
+    }).catch(next);
+});
+
 mongoose.connect(process.env.DB_URI,
     {useNewUrlParser: true,useUnifiedTopology: true,/*useFindAndModify: false*/}, 
     () => console.log('connected to db')
