6a4654bf0fe9a1ef757f762461d5005f8b38c982
RSarona <saronaraulen@gmail.com>
2021-07-18 17:52:57 +0800

    Added new file edit_contact.dart
    Added updating in the api (Unfinished)

--- phonebook_app/lib/api/api.dart
+++ phonebook_app/lib/api/api.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-final uri = Uri.parse('https://rs-phonebook.herokuapp.com/');
+const uri = 'https://rs-phonebook.herokuapp.com/';
+const Map<String, String> _header =  {
+        'Content-Type': 'application/json; charset=UTF-8',
+      };
 class contactAPI {
   Future<List> getContacts() async {
     final Map data;
-    http.Response response = await http.get(uri);
+    http.Response response = await http.get(Uri.parse(uri));
     data = jsonDecode(response.body);
     if (response.statusCode == 200) {
       return await data['fetchedInfo'];
@@ -15,18 +18,27 @@ class contactAPI {
 
   Future<String> deleteContact(String id) async {
     final Map data;
-    http.Response response = await http.delete(
-      Uri.parse('$uri$id'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
+    http.Response response = await http.delete(Uri.parse('$uri$id'),
+      headers: _header
     );
     data = jsonDecode(response.body);
-
     if (response.statusCode == 200) {
       return await data['fetchedInfo'];
     } else {
-      throw await Exception('Failed to load data');
+      throw await Exception('Failed to delete data');
+    }
+  }
+
+  Future<List> updateContact() async {
+    final Map data;
+    http.Response response = await http.post(Uri.parse(uri),
+      headers: _header
+    );
+    data = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      return await data['fetchedInfo'];
+    } else {
+      throw await Exception('Failed to update data');
     }
   }
 }
