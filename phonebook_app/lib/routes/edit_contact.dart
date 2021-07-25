216559f44010051ab41e267b09191e39d0253fdb
RSarona <saronaraulen@gmail.com>
2021-07-25 22:06:07 +0800

    Added clearTextFields Adjusted to ListView instead of SingleChildScrollView

--- phonebook_app/lib/routes/edit_contact.dart
+++ phonebook_app/lib/routes/edit_contact.dart
@@ -4,6 +4,8 @@ import 'package:phonebook/api/api.dart';
 class editContact extends StatelessWidget {
   final Map contact;
   const editContact({ Key? key, required this.contact }) : super(key: key);
+
+  
   
   @override
   Widget build(BuildContext context) {
@@ -20,74 +22,91 @@ class editContact extends StatelessWidget {
       _phoneNumbers[i].text = contact['phone_numbers'][i];
     }
     /*===================================================================================================*/
+
+    clearTextFields() {
+      _firstname.clear();
+      _lastname.clear();
+      for (var i = 0; i < _phoneNumbers.length; i++) {
+        _phoneNumbers[i].clear();
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Editing Contact'),
         centerTitle: true,
       ),
       // resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
+      body: Center(
+        child: ListView(
+          children: [
               Padding(
-                padding: const EdgeInsets.all(15),
-                child: CircleAvatar(
-                  radius: 42,
-                  child: Icon(Icons.edit,
-                    size: 45,
-                  ),
-                ),
-              ),
-              Divider(height: 20, color: Colors.blueGrey[900],),
-              SizedBox(height: 10),
-              TextField(
-                controller: _firstname,
-                decoration: InputDecoration(labelText: 'First Name'),
-              ),
-              TextField(
-                controller: _lastname,
-                decoration: InputDecoration(labelText: 'Last Name'),
-              ),
-              SizedBox(height: 25,),
-              Container(
-                height: 235,
-                child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  itemCount: contact['phone_numbers'].length,
-                  itemBuilder: (BuildContext context, int index){
-                    return TextField(
-                      controller: _phoneNumbers[index],
-                      decoration: InputDecoration(labelText: 'Phone Number #${index+1}'),
-                      keyboardType: TextInputType.number,
-                    );
-                  },
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+                child: Column(
                 children: <Widget>[
-                  TextButton(
-                    onPressed: () {}, 
-                    child: Text('Clear All',
-                      style: TextStyle(
-                        color: Colors.red,
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: CircleAvatar(
+                      radius: 40,
+                      child: Icon(Icons.person_add,
+                        size: 48,
                       ),
                     ),
                   ),
-                  TextButton(
-                    onPressed: () {}, 
-                    child: Text('Save Changes',
-                      style: TextStyle(
-                        color: Colors.green,
-                      ),
+                  Divider(height: 20, color: Colors.blueGrey[900],),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: _firstname,
+                    decoration: InputDecoration(hintText: 'Enter a first name', labelText: 'First Name'),
+                  ),
+                  TextField(
+                    controller: _lastname,
+                    decoration: InputDecoration(hintText: 'Enter a last name', labelText: 'Last Name'),
+                  ),
+                  SizedBox(height: 25,),
+                  Container(
+                    height: 240,
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      itemCount: contact['phone_numbers'].length,
+                      itemBuilder: (BuildContext context, int index){
+                        return TextField(
+                          controller: _phoneNumbers[index],
+                          decoration: InputDecoration(hintText: 'Enter a Number', labelText: 'Phone Number #${index+1}'),
+                          keyboardType: TextInputType.number,
+                        );
+                      },
                     ),
-                  )
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      TextButton(
+                        onPressed: clearTextFields, 
+                        child: Text('Clear All',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('+ Add Number'),
+                      ),
+                      TextButton(
+                        onPressed: () {}, 
+                        child: Text('Save Changes',
+                          style: TextStyle(
+                            color: Colors.green,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ],
+            ),
               ),
-            ],
-          ),
+          ], 
         ),
       ),
     );
